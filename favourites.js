135c22760d64389ff3f7e97efe75405618c05bf5
Carl Dunn <carl.dunn@roq.co.uk>
2023-02-09 11:37:40 +0000

    We've built the skeleton for our Favourites class

---
+++ favourites.js
@@ -0,0 +1,24 @@
+class Favourites {
+    //This is where the Array of favourite restaurants should be held
+    constructor(){
+
+    };
+
+    /*This function allows the user to cycle forwards and backwards through their 
+    favourites list using an input of either 'next' or 'previous' and return the current favourite*/
+    cycle(direction){
+        throw new Error('Favourites list not yet supported!');
+    };
+
+    //This function returns a random favourite restaurant for the Array
+    shuffle(){
+         throw new Error('Favourites list not yet supported!');  
+    };
+
+    //This function should add an input to the favourites array and return the new array
+    add(restaurantName){
+        throw new Error('Favourites list not yet supported!');
+    };
+}
+
+module.exports = Favourites;
